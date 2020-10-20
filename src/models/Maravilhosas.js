97f473d7acf6f6cfd1f23abd6d4e890cc4928dc0
luzialeticia <leticialuzia@tutamail.com>
2020-10-20 00:11:14 -0300

    Criando as funções para p update e delete

--- src/models/Maravilhosas.js
+++ src/models/Maravilhosas.js
@@ -1,9 +1,45 @@
 const data = require('../data/data.json')
 
 //selectAll
+const select = () => data
 
-const selectAll = () => data
+//selectById
+const selectById = (id) => {
+  const maravilhosa = data.find(maravilhosa => maravilhosa.id == id)
 
+  return maravilhosa
+}
 
+//insertData
+const insert = (itemToInsert) => {
+  data.push(itemToInsert)
 
-module.exports = selectAll
+  return itemToInsert
+}
+
+//updateData
+const update = (id, itemUpdated) => {
+  const maravilhosaToUpdate = data.find(maravilhosa => maravilhosa.id == id)
+
+  data.splice(maravilhosaToUpdate, 1, itemUpdated)
+
+  return itemUpdated
+}
+
+//deleteDate
+const deleteItem = (id) => {
+  const itemToDelete = data.find(maravilhosa => maravilhosa.id == id)
+  const index = data.indexOf(itemToDelete)
+
+  data.splice(index, 1)
+
+  return data
+}
+
+module.exports = {
+  select,
+  selectById,
+  insert,
+  update,
+  deleteItem
+}
